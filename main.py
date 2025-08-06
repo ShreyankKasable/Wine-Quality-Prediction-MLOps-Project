cfb55af9f45afbcb71948955c965b6adf2d2075b
Shreyank Kasable <shreyankkasabale@gmail.com>
2025-08-07 01:24:20 +0530

    Implemented Data Ingestion part Successfully

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from src.WineQuality import logger
+from src.WineQuality.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 
 logger.info(" Custom Logging Module is working fine")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.initiate_data_ingestion()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
