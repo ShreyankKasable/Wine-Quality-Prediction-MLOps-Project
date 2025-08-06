29652703981ca247070239e0d1a88b4bbb97a1be
Shreyank Kasable <shreyankkasabale@gmail.com>
2025-08-06 20:59:32 +0530

    Implemented Custom Logging Module

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.WineQuality import logger
+
+logger.info(" Custom Logging Module is working fine")
--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = "Wine-Quality"
+project_name = "WineQuality"
 
 list_of_files=[
 ".github/workflows/.gitkeep",
